65b20ff11b84525b0385aa0637349cbb05fce492
bhanu6097 <36534485+bhanu6097@users.noreply.github.com>
2018-08-21 09:16:54 +0530

    Increement

--- src/components/Restaurant/Counter.js
+++ src/components/Restaurant/Counter.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      itemCount: 0
+    };
+    this.increement = this.increement.bind(this);
+  }
+  increement = () => {
+    this.setState = { itemCount: this.state.itemCount + 1 };
+  };
   render() {
     return (
       <div>
-        <button>-</button>
-        {}
-        <button>+</button>
+        <button onClick="Decreement">-</button>
+        {this.state.itemCount}
+        <button onClick="increement">+</button>
       </div>
     );
   }
